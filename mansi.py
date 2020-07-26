ffad18cecc7ca8659478b3aa82b5749b55208a0d
unknown <mansiprajapati52@yahoo.com>
2020-07-26 10:56:31 +0530

    xyz

---
+++ mansi.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import warnings
+warnings.filterwarnings("ignore")
+from pylab import rcParams
+get_ipython().run_line_magic('matplotlib', 'inline')
+# Loading the CSV with pandas
+data = pd.read_csv('..Customer Churn/Telco-Customer-Churn.csv')
+# Data to plot
+sizes = data['Churn'].value_counts(sort = True)
+colors = ["grey","purple"] 
+rcParams['figure.figsize'] = 5,5
+# Plot
+plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+        autopct='%1.1f%%', shadow=True, startangle=270,)
+plt.title('Percentage of Churn in Dataset')
+plt.show()
+data.drop(['customerID'], axis=1, inplace=True)
+data['TotalCharges'] = pd.to_numeric(data['TotalCharges'])
+data["Churn"] = data["Churn"].astype(int)
+Y = data["Churn"].values
+X = data.drop(labels = ["Churn"],axis = 1)
+# Create Train & Test Data
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=101)
+from sklearn.linear_model import LogisticRegression
+model = LogisticRegression()
+result = model.fit(X_train, y_train)
+from sklearn import metrics
+prediction_test = model.predict(X_test)
+# Print the prediction accuracy
+print (metrics.accuracy_score(y_test, prediction_test))
+
+# To get the weights of all the variables
+weights = pd.Series(model.coef_[0],
+ index=X.columns.values)
+weights.sort_values(ascending = False)
+
+
+# In[ ]:
+
+
+
+
